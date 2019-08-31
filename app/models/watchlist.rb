a24d5874ef0b63dca9c5a52e895593524754f1f4
dhintz <dhintz89@gmail.com>
2019-08-31 15:24:02 -0400

    move rollup attributes from table to model methods to allow for join-table data structure

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,4 +1,13 @@
 class Watchlist < ApplicationRecord
     belongs_to :user
     belongs_to :watchlist
+
+    def total_value
+
+    end
+
+    def total_change
+
+    end
+
 end
--- db/migrate/20190829225724_create_watchlists.rb
+++ db/migrate/20190829225724_create_watchlists.rb
@@ -3,9 +3,6 @@ class CreateWatchlists < ActiveRecord::Migration[5.2]
     create_table :watchlists do |t|
       t.string :name
       t.string :description
-      t.integer :total_value
-      t.integer :total_change
-      t.integer :last_updated
       t.integer :user_id
       t.integer :security_id
 
